e98a34ecc61c4708012c287665894d483dc89f7e
Marco Saez <msaez@azken.com>
2025-07-08 12:41:32 +0200

    Proyecto finalizado

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'previews.123rf.com'
       },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com'
+      },
     ]
   }
 };
--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,4 +1,7 @@
+import { Pokemon } from './interfaces/pokemon';
 export { PokemonGrid } from './components/PokemonGrid';
 
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
+
+export type { Pokemon } from './interfaces/pokemon';
